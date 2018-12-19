7be040b6a4f80345c9344b3111b9dc8ce5ad912e
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2018-12-19 12:46:57 +0000

    Add exo3

--- examen2015 Partie1.c
+++ examen2015 Partie1.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#define MAX 100
 
 int* range(int t[], int n){
 	int *r = (int*)malloc(n*sizeof(int));
@@ -75,6 +76,83 @@ int nbVoisins(int g[][MAX], int n, int i, int j){
 return nb;
 }
 
+
+int estVivant(int g[][], int n, int i, int j){
+    int voisin = nbVoisin(g, n, i, j);
+    if(voisin == 0 || voisin == 4) return 0;
+    if(voisin == 2 || voisin == 3) return 0;
+    return g[i][j];
+}
+void changeEtat(int g[][], int n){
+    int i, j;
+    for(i = 1; i <= n ; i++){
+        for(j = 1; j <= n ; j++){
+            g[i][j] = estVivant(g, n, i, j);
+        }
+    }
+}
+
+char** newStringArray(int nb){
+    char** tab = (char**)malloc(nb*sizeof(char*));
+    int i;
+    for(i = 0 ; i < nb ; i++)
+        tab[i] = (char*)malloc(MAX*sizeof(char));
+return tab;
+}
+
+char** newCharMatrix(int size){
+    char** mat = (char**)malloc(size*sizeof(char*));
+    int i;
+    for(i = 0 ; i < size ; i++)
+        mat[i] = (char*)malloc(size*sizeof(char));
+return mat;
+}
+
+void printMatrix(int mat[][], int size){
+    int i, j;
+    for(i = 0; i < size ; i++){
+        for(j = 0; j < size ; j++){
+            printf("%c ", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+typedef struct indice{
+    int deb;
+    int fin;
+}Indices;
+
+Indices lineIndice(int mat[][], int size, int line, char mot[]){
+    int i, j;
+    int trouver = 0, deb = -1, fin = -1;
+    Indices res;
+    for(i = 0; i < size ; i++){
+        if(mat[line][i] == mot[0]){
+            trouver = 1;
+            deb = i;
+            for(j = 1; j < size && mat[line][i+j] == mot[j] ; j++){
+                trouver++;
+                fin = i+j;
+            }
+        }
+        if(trouver){
+            res.deb = deb;
+            res.fin = fin;
+        }
+    }
+return res;
+}
+
+void solve(int mat[][], int size){
+    int i, j;
+    for(i = 0; i < size ; i++){
+        for(j = 0; j < size ; j++){
+            printf("%c ", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
 int main(){
 
 	int t[8];
