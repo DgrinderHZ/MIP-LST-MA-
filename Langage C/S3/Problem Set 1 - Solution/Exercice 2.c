4e7b062d220ab7c78289bfd0dc9c0240e89aae4e
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2019-11-25 10:36:57 +0100

    Create Exercice 2.c

---
+++ Langage C/S3/Problem Set 1 - Solution/Exercice 2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int main(){
+	int a, b, n;
+
+	printf("Veuillez saisir le nombre a puis b: ");
+	scanf("%d%d", &a, &b);
+	
+	if((0 <= b && 0 <= a) || (0 >= b && 0 >= a))
+		printf("Le produit est positif.\n");
+	else
+		printf("Le produit est negatif.\n");
+	
+	
+	getch();
+	return 0;
+}
