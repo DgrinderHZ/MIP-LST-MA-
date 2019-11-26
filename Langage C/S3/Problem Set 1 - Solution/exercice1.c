8fdb0cea2c6c351b87eaf2c3042515f7654ad0a7
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2019-11-26 02:30:08 +0100

    Update exercice1.c

--- Langage C/S3/Problem Set 1 - Solution/exercice1.c
+++ Langage C/S3/Problem Set 1 - Solution/exercice1.c
@@ -6,17 +6,17 @@
 int main(){
 	int a, b, n;
 
-	printf("Veuillez saisir le nombre a puis b: [a, b]");
+	printf("Veuillez saisir le nombre a puis b: [a, b]\n");
 	scanf("%d%d", &a, &b);
-	printf("Veuillez \nsaisir le nombre n:\n");
+	printf("Veuillez saisir le nombre n:\n");
 	scanf("%d", &n);
 	
-	if((n <= b && n >= a) || (n <= b && n >= a))
+	if(n <= b && n >= a)
 		printf("%d appartient a [%d, %d].\n", n, a, b);
 	else
-		printf("%d n' appartient pas a [%d, %d].\n", n, a, b);
+		printf("%d n'appartient pas a [%d, %d].\n", n, a, b);
 	
 	
-	//getch();
+	getch();
 	return 0;
 }
