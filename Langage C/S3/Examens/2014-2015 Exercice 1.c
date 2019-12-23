d0bdbcb4c22c2454f725d731cdddf0b8132a758b
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2019-12-24 00:51:44 +0100

    Update 2014-2015 Exercice 1.c

--- Langage C/S3/Examens/2014-2015 Exercice 1.c
+++ Langage C/S3/Examens/2014-2015 Exercice 1.c
@@ -1,11 +1,15 @@
 // 1)
 int* range(int t[], int n){
-	int *r = (int*)malloc(n*sizeof(int));
+	/* Prend un tableau t et sa longueur n en paramètres
+	   et retourne un tableau r tel que r[i] compte le 
+	   nombre de t[j] inférieur à t[i]!
+	   */
+	int *r = (int*)malloc(n*sizeof(int)); // allocation dynamique
 	int i, j;
-	for ( i = 0; i < n; ++i)
+	for ( i = 0; i < n; ++i) // fixer t[i]
 	{
-		int cpt = 0;
-		for ( j = 0; j < n; ++j)
+		int cpt = 0; // compter combien de t[j] < t[i]
+		for ( j = 0; j < n; ++j) 
 		{
 			if(t[j] < t[i]) cpt++;
 		}
@@ -16,6 +20,10 @@ return r;
 
 // 2)
 int* tri(int t[], int r[], int n){
+	/* Prend deux tableau t et r avec leur longueur n en paramètres
+	   et retourne un tableau tt représentant la version triée
+	   de t!
+	   */
 	int *tt = (int*)malloc(n*sizeof(int));
 	int i, pos;
 	for (i = 0; i < n; ++i)
